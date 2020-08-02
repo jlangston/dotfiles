557832a7400570003bec8a5f0c54955c2f5a14f9
Jeff  Langston <jrlangston@gmail.com>
2020-08-01 20:56:27 -0600

    adding fzf delete branch

--- fzf-snippets.zsh
+++ fzf-snippets.zsh
@@ -81,6 +81,14 @@ fbr() {
   git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
 }
 
+# fbd - delete git branch (including remote branches)
+fbd() {
+  local branches branch
+  branches=$(git for-each-ref --count=30 --sort=-committerdate refs/heads/ --format="%(refname:short)") &&
+  branch=$(echo "$branches" | fzf --multi ) &&
+  git branch -D $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
+}
+
 # fco - checkout git branch/tag
 fco() {
   local tags branches target
@@ -243,3 +251,5 @@ function ds() {
 
   [ -n "$cid" ] && docker stop "$cid"
 }
+
+
